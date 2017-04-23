e3b55ecfa9bb2afae6a7f8133f3b3b0eb922cc27
MMasoodAli <ubuntu@ip-10-0-1-131.ec2.internal>
2017-04-23 09:19:15 +0000

    updatedchanges

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,9 @@
-# Path, relative to the root of the git repository, where data bags are stored
-default['delivery']['config']['delivery-bag']['data-bag-repo-path'] = ""
+# Path where data bags and chef config file are stored
+default['delivery']['config']['delivery-bag']['data-bag-repo-path'] 				= "/home/ubuntu/test-repo/data_bags"
+default['delivery']['config']['delivery-bag']['json-path'] 							= "/home/ubuntu/test-repo/data_bags/test-bag/test.json"
+default['delivery']['config']['delivery-bag']['data-bag-path'] 						= "/home/ubuntu/test-repo/data_bags/test-bag"
+default['delivery']['config']['delivery-bag']['data-bag-name'] 						= "test-bag"
+default['delivery']['config']['knife_rb'] 											= "/etc/chef/knife.rb"
+default['delivery']['config']['git-repo-path']										= "/home/ubuntu/test-repo"
+default['delivery']['config']['git-repo-uri']										= "git@github.com:gitsite/deployment.git"
+
--- recipes/lint.rb
+++ recipes/lint.rb
@@ -1,18 +1,20 @@
-#
-# Copyright:: Copyright (c) 2015 Chef Software, Inc.
-# License:: Apache License, Version 2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+###############################################################################
+# This recipe validates the json files present in a data_bag.
+##
+data_bag_path = node['delivery']['config']['delivery-bag']['data-bag-path'] 
+Dir.chdir(data_bag_path)
+databag_items = Dir.glob("*").select {|f| File.directory? f}
 
-# This recipe is intentionally left blank
+databag_items.each do |file|
+  begin
+    contents = File.open(file, 'rb').read
+    bag = JSON.parse(contents)
+  rescue JSON::ParserError
+    raise "#{file} not valid JSON!"
+  end
+  if bag.fetch('id') != File.basename(file, '.json')
+    fail "#{file} has mismatched 'id' attribute!"
+  end
+end
+
+
